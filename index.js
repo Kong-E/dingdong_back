ba0edbb3994d6b85c92f6050e9688966896cd324
Kong-E <kng001016@dgu.ac.kr>
2023-09-01 07:17:56 +0900

    fix: cors 코드 수정

--- index.js
+++ index.js
@@ -34,18 +34,23 @@ mongoose
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
 
-/* // Preflight 요청 처리
-app.options("*", cors()); */
+const allowedOrigins = [
+  "https://dingdong-front.vercel.app",
+  "http://localhost:3001",
+];
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3001");
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (allowedOrigins.includes(origin) || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 
 app.use("/admin", adminRoute);
 app.use("/api/auth", authRoute);
