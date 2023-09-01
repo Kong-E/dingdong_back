fb095ffa2224b12488f42027ac1ec0e3e2e28623
Kong-E <kng001016@dgu.ac.kr>
2023-09-01 13:12:36 +0900

    ..

--- index.js
+++ index.js
@@ -25,6 +25,14 @@ app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(passport.initialize());
 app.use(cookieParser());
 app.use(express.json({ limit: "50mb" }));
+const allowedOrigins = "http://localhost:3001";
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
 
 mongoose
   .connect(process.env.MONGO_URL, {
@@ -33,11 +41,6 @@ mongoose
   })
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
-
-const allowCors = require("./middleware/cors");
-
-// Use the allowCors middleware for localhost:3001
-app.use(allowCors);
 
 app.use("/admin", adminRoute);
 app.use("/api/auth", authRoute);
