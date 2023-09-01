697f4da255dd671176aa47a788aabb770d53b3f2
Kong-E <kng001016@dgu.ac.kr>
2023-09-01 14:25:21 +0900

    origin 변경

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(passport.initialize());
 app.use(cookieParser());
 app.use(express.json({ limit: "50mb" }));
-const allowedOrigins = "http://localhost:3001";
+const allowedOrigins = "https://dingdong-front.vercel.app'";
 
 app.use(
   cors({
--- middlewares/allowCors.js
+++ middlewares/allowCors.js
@@ -1,6 +1,9 @@
 const allowCors = (fn) => async (req, res) => {
   res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3001");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://dingdong-front.vercel.app'"
+  );
   // another common pattern
   // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
   res.setHeader(
